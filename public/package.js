c9917d177b9ace8b5d3d8b7ea9bdf776a4761174
naifAlbalawi <naifxbl99@gmail.com>
2022-12-14 10:02:39 +0300

    payment

--- public/package.js
+++ public/package.js
@@ -115,7 +115,7 @@ function editPackage(pkg_num) {
     });
   });
 
-<<<<<<< HEAD
+
 function pay(package_number,Weight,Height,Width,Length,value,Customer_SSN){
   document.getElementById('value').innerText=(Weight*10/100+Height*Width*Length*10/100+value*20/100)+' SR'
   document.getElementById('delivery_fee').innerText=(Weight*10/100+Height*Width*Length*10/100)+' SR'
@@ -129,16 +129,4 @@ function pay(package_number,Weight,Height,Width,Length,value,Customer_SSN){
   }
   
   document.getElementById('modal').showModal()
-=======
-  function pay(package_number, Weight, Height, Width, Length, value, Customer_SSN) {
-    document.getElementById("value").innerText =
-      "the payent value including the insurance amount is " + ((Weight * 10) / 100 + (Height * Width * Length * 10) / 100 + (value * 20) / 100);
-    document.getElementById("pay-btn").addEventListener("click", () => {
-      document.getElementById("modal").close();
-      Package_payed(package_number, Customer_SSN);
-    });
-    console.log("opened");
-    document.getElementById("modal").showModal();
-  }
->>>>>>> 5927bf4842bd8f98152937e854c0ac362f2c8421
-}
+}}
