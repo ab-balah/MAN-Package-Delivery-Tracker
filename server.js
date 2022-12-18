211246f8e785bd3f518e4d56d43189332062a91c
a-balah <109286462+ab-balah@users.noreply.github.com>
2022-12-18 23:52:16 +0300

    Fixed packagePage functionality

--- server.js
+++ server.js
@@ -181,7 +181,7 @@ app.get("/Track/:Package_number", (req, res) => {
 app.get("/package/:Package_number", isLoggedIn, (req, res) => {
   var Package = Functions.getPackagesInfoByNumber(req.params.Package_number);
 
-  if (req.session.role === "Customer" && Package[0].Sender_SSN !== req.session.ssn && Package[0].Receiver_SSN !== req.session.ssn) {
+  if (req.session.role === "Customer" && Package[0]?.Sender_SSN !== req.session.ssn && Package[0]?.Receiver_SSN !== req.session.ssn) {
     return res.status(403).send("You cannot access this page");
   }
   var Track = Functions.TrackPackage(req.params.Package_number);
